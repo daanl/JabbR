968473bffa3136d2e46211cef3c4e66da1883714
Daan Le Duc <daanleduc@hotmail.com>
2014-05-21 18:21:40 +0200

    Started on angularify jabbr

--- JabbR/Scripts/app/app.js
+++ JabbR/Scripts/app/app.js
@@ -1,12 +1,5 @@
 ﻿/// <reference path="../angular.js" />
 /// <reference path="../angular-resource.js" />
-
-// Ensure calls to console.log don't break IE
-if (typeof console === "undefined" || typeof console.log === "undefined") {
-    console = {};
-    console.log = function () { };
-}
-
 var app = angular.module('jabbrApp', [
     'ngRoute',
     'ngResource',
@@ -19,71 +12,89 @@ var app = angular.module('jabbrApp', [
         controller: 'LobbyController'
     });
 })
-.controller('LobbyController', ['$scope', '$sanitize', '$window', function ($scope, $sanitize, $window) {
-    var connection = $window.jQuery.connection;
-    var chat = connection.chat;
-    var ui = $window.chat.ui;
-    var $ui = $(ui);
+.constant('jabbrConnection', window.jQuery.connection)
+.constant('jabbrChat', window.chat)
+.constant('$ui', $(window.chat.ui))
+.service('roomService', ['jabbrChat', function (jabbrChat) {
+            
+    return {
+        joinRoom: function(room) {
+            var $ui = $(jabbrChat.ui);
+            $ui.trigger(jabbrChat.ui.events.openRoom, [room.Name]);
+        }
+    }
+}])
+.constant('translate', function (resource) {
+    return window.chat.utility.getLanguageResource(resource);
+})
+.controller('LobbyController', ['$scope', '$log', 'jabbrConnection', 'roomService', function ($scope, $log, jabbrConnection) {
 
-    $scope.title = 'Lobby';
+    $scope.title = 'Lobby'; // Todo: this should be removed
     $scope.rooms = [];
     $scope.roomSearchText = '';
     $scope.showClosedRooms = false;
     $scope.pageSize = 100;
     $scope.pagesShown = 1;
 
-    $scope.itemsLimit = function () {
-        return $scope.pageSize * $scope.pagesShown;
-    };
+    $scope.privateRooms = [];
+    $scope.publicRooms = [];
 
-    $scope.showMoreItems = function () {
-        $scope.pagesShown++;
-    }
+    // Todo: make a filter for private rooms instead of this long function
+    $scope.$watch('rooms | filter:{Private: true} | filter:roomSearchText | filter:(showClosedRooms || !showClosedRooms && room.Closed) | limitTo: itemsLimit()', function (selectedCompanies) {
+        $scope.privateRooms = selectedCompanies;
+    }, true);
 
-    $scope.joinRoom = function (event, room) {
-        console.log('Joining room: ' + room.Name);
-        $ui.trigger(ui.events.openRoom, [room.Name]);
-    };
+    // Todo: make a filter for public rooms instead of this long function
+    $scope.$watch('rooms | filter:{Private: false} | filter:roomSearchText | filter:(showClosedRooms || !showClosedRooms && room.Closed) | limitTo: itemsLimit()', function (selectedCompanies) {
+        $scope.publicRooms = selectedCompanies;
+    }, true);
 
-    connection.hub.stateChanged(function (change) {
-        console.log(change.newState);
-        if (change.newState === connection.connectionState.connected) {
-            console.log('Connected')
-            chat.server.getRooms()
+    jabbrConnection.hub.stateChanged(function (change) {
+
+        $log.info(change.newState);
+
+        if (change.newState === jabbrConnection.connectionState.connected) {
+
+            $log.info('Connected');
+
+            jabbrConnection.chat.server.getRooms()
                 .done(function (rooms) {
-                    console.log('getRooms');
-                    console.log(rooms.length);
-                    angular.forEach(rooms, function (value, key) {
-                        console.log(value);
-                        value.getUserCount = function () {
-                            if (this.Count === 0)
-                                return $window.chat.utility.getLanguageResource('Client_OccupantsZero');
-                            else
-                                return (this.Count === 1 ? $window.chat.utility.getLanguageResource('Client_OccupantsOne') : this.Count + ' ' + $window.chat.utility.getLanguageResource('Client_OccupantsMany'));
-                        }
-                        $scope.rooms.push(value);
-                        $scope.$apply();
-                    });
+
+                    $log.info('getRooms returned: ' + rooms.length);
+                    $scope.rooms = rooms;
+                    $scope.$apply();
                 })
                 .fail(function (e) {
-                    console.log('getRooms failed: ' + e);
+                    $log.error('getRooms failed: ' + e);
                 });
         }
     });
 }])
-.controller('LobbyPublicRoomsController', ['$scope', '$window', '$filter', function($scope, $window, $filter) {
+.controller('LobbyPublicRoomsController', ['$scope', 'roomService', function ($scope, roomService) {
+
     $scope.isPrivate = false;
-    $scope.title = $window.chat.utility.getLanguageResource('Client_OtherRooms');
-    $scope.loadMoreTitle = $window.chat.utility.getLanguageResource('Client_LoadMore');
+
     $scope.hasMoreItems = function () {
         return $scope.pagesShown < ($scope.rooms.length / $scope.pageSize);
     };
+
+    $scope.joinRoom = function (event, room) {
+        $log.info('Joining room: ' + room.Name);
+        roomService.joinRoom(room);
+    };
+
 }])
-.controller('LobbyPrivateRoomsController', ['$scope', '$window', '$filter', function ($scope, $window, $filter) {
+.controller('LobbyPrivateRoomsController', ['$scope', 'roomService', function ($scope, roomService) {
+
     $scope.isPrivate = true;
-    $scope.title = $window.chat.utility.getLanguageResource('Client_Rooms');
+
     $scope.itemsLimit = function () {
         return $scope.rooms.length;
+    };
+
+    $scope.joinRoom = function (event, room) {
+        $log.info('Joining room: ' + room.Name);
+        roomService.joinRoom(room);
     };
 }])
 .directive('jabbrLobby', function () {
@@ -92,9 +103,18 @@ var app = angular.module('jabbrApp', [
         templateUrl: 'Scripts/app/areas/rooms/lobby.html'
     };
 })
-.directive('jabbrLobbyRooms', function () {
+.directive('jabbrLobbyRooms', ['$log', 'translate', function ($log, roomService) {
     return {
         restrict: 'A',
+        scope: {
+            rooms: '=',
+            isPrivate: '=',
+        },
         templateUrl: 'Scripts/app/areas/rooms/lobby-rooms.html',
     }
-});
+}])
+.run(['$rootScope', 'translate', function ($rootScope, translate) {
+
+    // Set translate function on rootScope so we can access it in the view like t(code)
+    $rootScope.t = translate;
+}]);
